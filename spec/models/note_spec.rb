7b710974f8d12e797bcceceb7e5c46622ae8a30c
yuuwatanabe <panyoriokome@gmail.com>
2022-05-11 10:00:00 +0900

    factoryの途中まで

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  # ファクトリで関連するデータを生成する
+  it "generates associated data from a factory" do
+    note = FactoryBot.create(:note)
+    puts "This note's project is #{note.project.inspect}"
+    puts "This note's user is #{note.user.inspect}"
+  end
 
   # このファイルの全テストで使用するテストデータをセットアップする
   before do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,42 +20,57 @@ RSpec.describe User, type: :model do
 
   # 名がなければ無効な状態であること
   it "is invalid without a first name" do
-    user = User.new(first_name: nil)
+    user = FactoryBot.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
   # 姓がなければ無効な状態であること
   it "is invalid without a last name" do
-    user = User.new(last_name: nil)
+    user = FactoryBot.build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
   # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address"
+  it "is invalid without an email address" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
   # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email address" do
-    User.create(
-      first_name:  "Joe",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-    user = User.new(
-      first_name:  "Jane",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
+    # User.create(
+    #   first_name:  "Joe",
+    #   last_name:  "Tester",
+    #   email:      "tester@example.com",
+    #   password:   "dottle-nouveau-pavilion-tights-furze",
+    # )
+    # user = User.new(
+    #   first_name:  "Jane",
+    #   last_name:  "Tester",
+    #   email:      "tester@example.com",
+    #   password:   "dottle-nouveau-pavilion-tights-furze",
+    # )
+    # 重複を確認するためにはDBにモデルが保存されている必要がある
+    # そのためにbuildではなくcreateを使う
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
   # ユーザーのフルネームを文字列として返すこと
   it "returns a user's full name as a string" do
-    user = User.new(
-      first_name: "John",
-      last_name: "Doe",
-      email: "johndoe@example.com",
-    )
+    # user = User.new(
+    #   first_name: "John",
+    #   last_name: "Doe",
+    #   email: "johndoe@example.com",
+    # )
+    # expect(user.name).to eq "John Doe"
+    user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
     expect(user.name).to eq "John Doe"
+  end
+  it "does something with multiple users" do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.create(:user)
+    expect(true).to be_truthy
   end
 end
